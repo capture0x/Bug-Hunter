da266d0dca20cda10cd5a63c3b3cb773cd7d1604
tmrswrr <61238348+capture0x@users.noreply.github.com>
2025-01-21 09:25:23 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux-i386.zip && unzip findomain-linux-i386.zip && chmod +x findomain && sudo mv findomain /usr/bin/findomain
+curl -LO https://github.com/projectdiscovery/httpx/releases/download/v1.6.9/httpx_1.6.9_linux_amd64.zip && unzip httpx_1.6.9_linux_amd64.zip && chmod +x httpx
+git clone https://github.com/projectdiscovery/nuclei-templates.git && echo "Nuclei templates downloaded successfully."
